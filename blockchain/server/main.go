ebe7b84c88b92939a1a46c971bab3032942a272f
root <root@Aria-Rose.localdomain>
2019-10-14 00:20:58 -0700

    implement service

--- blockchain/server/main.go
+++ blockchain/server/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net"
+	"context"
 	"google.golang.org/grpc"
 
 
 	"github.com/archacon/chacoworks/blockchain/proto"
+	"github.com/archacon/chacoworks/blockchain/server/blockchain"
 )
 func main() {
 	listener, err := net.Listen("tcp", "8080")
@@ -16,4 +18,32 @@ func main() {
 	}
 
 	srv  := grpc.NewServer()
+	proto.RegisterBlockchainServer(srv, &Server{
+		Blockchain: blockchain.NewBlockchain
+	})
+	srv.Serve(listener)
+}
+
+type Server struct {
+	BlockChain *blockchain.Blockchain
+}
+
+func (s *Server) AddBlock(ctx context.Context, in *proto.AddBlockRequest) (*proto.AddBlockResponse, error) {
+	block := s.Blockchain.AddBlock(in.Data)
+
+	return new(proto.AddBlockResponse{
+		Hash: block.Hash, 
+	}),nil
+}
+
+func (s *Server) GetBlockchain(ctx context.Context, in *proto.GetBlockchainRequest) (*proto.GetBlockchainResponse, error) {
+	resp := new(proto.GetBlockchainResponse)
+	for _, block := range s.Blockchain.Blocks {
+		resp.Blocks = append(resp.Blocks, &proto.Block{
+			PrevBlockHash: block.PrevBlockHash,
+			Hash: block.Hash,
+			Data: block.Data,
+		})
+	}
+	return resp,nil
 }
